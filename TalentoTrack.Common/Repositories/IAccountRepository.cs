0d3163ec5cf42199f864da5b4ab922098df324b2
Bapu Pawar <bapupawar101@gmail.com>
2023-09-29 17:00:43 +0530

    - Initial commit - Implemented base achitecture - Created Login web API

---
+++ TalentoTrack.Common/Repositories/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using TalentoTrack.Common.Entities;
+
+namespace TalentoTrack.Common.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<LoginDetails> GetLoginDetails(string userName, string password);
+    }
+}
